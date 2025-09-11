874593d46baccfdb9d290bb097a13d751e2b0d20
Fearghal <146260233+FearghalM@users.noreply.github.com>
2025-09-11 19:47:51 +0000

    feat: add semantic-ui-css dependency and implement geolocation in App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1>Clear Sky</h1>
+      <h2>Latitude: {lat}</h2>
+      <h2>Longitude: {lon}</h2>
     </div>
   );
 }
-
-export default App;
