2ccaf257b2e15d9b08ac0204ddc692d569723541
Fearghal <146260233+FearghalM@users.noreply.github.com>
2025-09-14 09:50:33 +0000

    fix: update geolocation handling and state management in App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from './components/Weather';
 
 export default function App() {
-  const [lat, setLat] = useState(null);
-  const [lon, setLon] = useState(null);
-
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
-    });
-  }, []);
+     const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+      });
+
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+      fetchData();
+  }, [lat, long]);
 
   return (
     <div className="App">
       <h1>Clear Sky</h1>
       <h2>Latitude: {lat}</h2>
-      <h2>Longitude: {lon}</h2>
+      <h2>Longitude: {long}</h2>
     </div>
   );
 }
